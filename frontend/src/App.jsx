9bebd6c85daabb7a0a90798c9309fc7abbf81285
Youngjae <2015fromjeki@gmail.com>
2025-05-17 11:41:38 +1000

    Feat(Front-end): Finalised React Router set up

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,17 @@
+import HomePage from "./pages/HomePage"
+import ProductPage from "./pages/ProductPage"
+import Navbar from "./components/Navbar"
+import { Routes, Route } from "react-router-dom"
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline btn">
-      Hello world!
-    </h1>
+    <div className="min-h-screen bg-base-200 transition-colors duration-300">
+      {/* UI Consistency */}
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/product/:id" element={<ProductPage />} />
+      </Routes>
+    </div>
   )
 }
